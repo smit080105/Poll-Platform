9ff5422a48e4d9136c528747e9fa7d6eac7357c2
smit080105 <smitbhondwe3@gmail.com>
2026-09-09 11:03:41 +0530

    Add helmet middleware for standard security headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import pollRoutes from './routes/polls.js';
@@ -22,6 +23,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
+app.use(helmet());
 app.use(express.json());
 
 // Make io accessible to routes
